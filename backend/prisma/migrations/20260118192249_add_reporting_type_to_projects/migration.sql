68f27ffec2df8f0c4cd6264c0d5cc0419aa3180d
Dor Haboosha <134487550+dorhaboosha@users.noreply.github.com>
2026-01-22 12:59:07 +0200

    updating the packages

--- backend/prisma/migrations/20260118192249_add_reporting_type_to_projects/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "ReportingType" AS ENUM ('duration', 'startEnd');
-
--- AlterTable
-ALTER TABLE "projects" ADD COLUMN     "reporting_type" "ReportingType" NOT NULL DEFAULT 'startEnd';
--- backend/prisma/migrations/20260119185437_add_timelog_start_end/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "project_time_logs" ADD COLUMN     "end_time" TIME,
-ADD COLUMN     "start_time" TIME;
